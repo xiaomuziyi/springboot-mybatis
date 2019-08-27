dae4f9e34bc4f3184d3fc0cc6e9697c134007599
offcn-java <382833278@qq.com>
2019-08-27 16:54:52 +0800

    Initial commit

---
+++ src/main/java/com/offcn/SpringbootMybatisApplication2.java
@@ -0,0 +1,13 @@
+package com.offcn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisApplication2 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisApplication2.class, args);
+    }
+
+}
---
+++ src/main/java/com/offcn/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.offcn.controller;
+
+
+import com.offcn.po.User;
+import com.offcn.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    //添加用户
+    @PostMapping("/")
+    public String save(User user){
+        userService.createUser(user);
+        return "success";
+    }
+
+    //获取用户
+    @GetMapping("/")
+    public List<User> getAll(){
+        return userService.getUserList();
+    }
+
+    //根据id获取用户
+    @GetMapping("/{id}")
+    public User getByid(@PathVariable(value = "id") Long id){
+        return userService.getUser(id);
+    }
+
+    //修改用户
+    @PutMapping("/{id}")
+    public String update(User user,@PathVariable(name = "id")Long id){
+        userService.updateUser(id,user);
+        return "success";
+    }
+
+    //删除用户
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id")Long id){
+        userService.deleteUser(id);
+        return "success";
+    }
+
+
+
+}
